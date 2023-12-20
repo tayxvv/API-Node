4af1f01d4c359bd3c70551680774c4a4f9282ceb
Tayssa Victoria <tayssa@ipeweb.com.br>
2023-12-20 13:44:44 -0300

    changes

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationController.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationController.ts
@@ -1,22 +1,23 @@
 import { container } from "tsyringe";
 import { CreateCarSpecificationUseCase } from "./CreateCarSpecificationUseCase";
-
+import { Request, Response } from "express";
 
 class CreateCarSpecificationController {
-    async handle(request: Request, response: Response) {
+  async handle(request: Request, response: Response) {
+    const { id } = request.params;
+    const { specifications_id } = request.body;
 
-        const { id } = request.params;
-        const { specifications_id } = request.body;
+    const createCarSpecificationUseCase = container.resolve(
+      CreateCarSpecificationUseCase
+    );
 
-        const createCarSpecificationUseCase = container.resolve(CreateCarSpecificationUseCase);
+    const cars = await createCarSpecificationUseCase.execute({
+      car_id: id,
+      specifications_id,
+    });
 
-        const cars = await createCarSpecificationUseCase.execute({
-            car_id: id,
-            specifications_id
-        });
-
-        return response.json(cars);
-    }
+    return response.json(cars);
+  }
 }
 
 export { CreateCarSpecificationController };
